1f63ec058a8ec5a47980f67190beea73a52345b5
iLewisBrogan <06016658@uhi.ac.uk>
2022-05-02 15:51:51 +0100

    intial setup for starting project

--- main.cpp
+++ main.cpp
@@ -1,43 +1,24 @@
 #include <SDL2/SDL.h>
-#include <iostream>
-
-int main(int argv, char** args)
-{
-	SDL_Init(SDL_INIT_EVERYTHING);
-
-	SDL_Window *window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-
-	bool isRunning = true;
-	SDL_Event event;
-
-	while (isRunning)
-	{
-		while (SDL_PollEvent(&event))
-		{
-			switch (event.type)
-			{
-			case SDL_QUIT:
-				isRunning = false;
-				break;
-
-			case SDL_KEYDOWN:
-				if (event.key.keysym.sym == SDLK_ESCAPE)
-				{
-					isRunning = false;
-				}
-			}
-		}
-
-		SDL_RenderClear(renderer);
-		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-
-		SDL_RenderPresent(renderer);
-	}
-
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-
-	return 0;
+ 
+int main(int argc, char* argv[]) {
+    // Start SDL2
+    SDL_Init(SDL_INIT_EVERYTHING);
+ 
+    // Create a Window in the middle of the screen
+    SDL_Window *window = 0;
+ 
+    window = SDL_CreateWindow("Hello World!",
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              640, 480,
+                              SDL_WINDOW_SHOWN);
+ 
+    // Delay so that we can see the window appear
+    SDL_Delay(2000);
+ 
+    // Cleanup and Quit
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+ 
+    return 0;
 }
